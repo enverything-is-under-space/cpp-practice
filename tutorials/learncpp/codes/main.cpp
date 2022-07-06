d974d1aba8ef7d65a5f48ea263a2857cde63600f
mrx <albin@senioradministrator.com>
2022-07-06 03:31:57 +0200

    book simple added

---
+++ tutorials/learncpp/codes/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+void donothing(int&)
+{
+
+}
+
+int main()
+{
+    // int x={2};
+    // cout << x;
+
+    // int x,y;
+    // cout << " enter numbs " ;
+    // cin >> x >> y;
+
+    // int x{};
+    // cout << x << endl;
+    // int y;
+    // cout << y << endl;
+    // int z=0;
+    // cout << z << endl;
+
+    // int x;
+    // donothing(x);
+
+
+    
+    return false;
+}
