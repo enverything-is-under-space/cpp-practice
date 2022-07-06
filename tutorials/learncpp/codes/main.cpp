d2a65d66a881fec7f8b0bbe6581238e299d3a624
mrx <albin@senioradministrator.com>
2022-07-07 01:57:06 +0200

    progma

--- tutorials/learncpp/codes/main.cpp
+++ tutorials/learncpp/codes/main.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
+#include "my.h"
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
+
+int add(int,int);
+
 void donothing(int&)
 {
 
 }
 
+int five()
+{
+    return 5;
+}
 int main()
 {
     // int x={2};
@@ -24,6 +32,62 @@ int main()
 
     // int x;
     // donothing(x);
+
+    // cout << "Hello"
+    //         "world";
+
+
+
+    // cout << "hello 
+    // world";
+
+
+    // int a{2*5+(66/5)};
+    // cout << a;
+    // int b{five()};
+    // cout << b;
+
+    // int x=2,y=2;
+    // cout << add(x,y);
+
+
+
+
+
+    // #define version1
+    // #define version2
+    // #ifdef version1
+    //     cout << "this is verison 1" << endl;
+    // #endif
+
+    // #ifdef version2
+    //     cout << "this is version 2 " << endl;
+    // #endif
+
+
+
+    // #define version1
+    // #define version2
+    // #ifdef version1
+    //     cout << "version 1 is defined" << endl;
+    // #endif
+
+    // #ifndef version2
+    //     cout << "version 2 is not difenied but its automaticily from ifndef code preproccseosr"<< endl;
+    // #endif
+
+
+
+    // #if 0
+    //     cout << "never print this hooh" << endl;
+    // #endif
+
+    // cout << "helo" << retme(50) <<endl;
+
+
+    //#pragma once ERRORRRR
+    //#progma
+
 
 
     
---
+++ tutorials/learncpp/codes/my.h
@@ -0,0 +1,15 @@
+#ifndef myheader
+#define myheader
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int retme(int x)
+{
+    return x;
+}
+
+#endif
